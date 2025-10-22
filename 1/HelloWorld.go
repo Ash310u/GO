fc44b2a2a49dc2702be0e14a2331ea6ad03e56b6
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-10-23 02:50:22 +0530

    update 1

--- 1/HelloWorld.go
+++ 1/HelloWorld.go
@@ -6,4 +6,9 @@ func main() {
 	// single-line comments start with "//"
 	// comments are just for documentation - they don't execute
 	fmt.Println("hello world")
+
+	// To append data in Go, use the built-in append function, usually with slices.
+	s := []string{"hello"}
+	s = append(s, "world")
+	fmt.Println(s) // Output: [hello world]
 }
