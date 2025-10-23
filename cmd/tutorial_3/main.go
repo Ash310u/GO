d5a4524350bf34d7a7b33203e50a22cdd15f20a3
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-10-23 23:11:01 +0530

    Printf and multi returned values

--- cmd/tutorial_3/main.go
+++ cmd/tutorial_3/main.go
@@ -8,8 +8,13 @@ func main() {
 	PrintMe("What do you want to divide:")
 	fmt.Scan(&numerator, &denominator)
 
-	result := intDivision(numerator, denominator)
+	result, quotient := intDivisionAndQuotient(numerator, denominator)
+	// Using Print line: Println
 	fmt.Println("Result:", result)
+	fmt.Println("Quotient:", quotient)
+
+	// Using Printf
+	fmt.Printf("The result of the integer division is %v with remainder %v\n", result, quotient)
 }
 
 func PrintMe(printValue string) {
@@ -20,4 +25,10 @@ func PrintMe(printValue string) {
 // Here is the function demonstrating the return type:
 func intDivision(numerator float32, denominator float32) float32 { // 'int' indicates the function returns an int
 	return numerator / denominator
+}
+
+func intDivisionAndQuotient(numerator float32, denominator float32) (float32, int) {
+	divident := numerator / denominator
+	quotient := int(numerator) % int(denominator)
+	return divident, quotient
 }
